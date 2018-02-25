741524f3bf27496b48d4498ea0936be6b2b481c9
ZhekaPrice <36643027+ZhekaPrice@users.noreply.github.com>
2018-02-25 23:44:19 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,30 +1,50 @@
 class Sorter {
   constructor() {
-    // your implementation
+   this.myArr = [];
+   this.funct = function sort (Arr,Arr1)
+   {
+     return Arr1 - Arr;
+   }
   }
 
   add(element) {
-    // your implementation
+    this.myArr.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.myArr[index];
   }
 
   get length() {
-    // your implementation
+    return this.myArr.push();
   }
 
   toArray() {
-    // your implementation
+    return this.myArr;
   }
 
   sort(indices) {
-    // your implementation
+    var tempArr = [];
+    for (var i = 0; i < indices.length; i++)
+      tempArr.unshift(this.myArr[indices[i]]);
+    for (var j = 0; j < tempArr.length - 1;j++)
+    {
+      for(var k = 0; k < tempArr.length - j; k++)
+      {
+        if(tempArr[k] > tempArr[k+1])
+        {
+          var buff = tempArr[k];
+          tempArr[k] = tempArr[k+1];
+          tempArr[k+1] = buff;
+        }
+      }
+    }
+    for (var i = 0; i < indices.length; i++)
+      this.myArr[indices[i]] = tempArr[i];
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.funct = compareFunction;
   }
 }
 
